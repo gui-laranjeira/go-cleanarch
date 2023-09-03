c2c316ee9b6b1686353c2acde1da3a145b87de7a
Guilherme Laranjeira <guilr14@gmail.com>
2023-09-03 11:27:55 -0300

    feat: add create user usecase

---
+++ internal/usecases/user/create_user.go
@@ -0,0 +1,42 @@
+package usecases
+
+import "github.com/gui-laranjeira/go-cleanarch/internal/entity"
+
+type CreateUserInput struct {
+	Email     string
+	Password  string
+	FirstName string
+	LastName  string
+}
+
+type CreateUserOutput struct {
+	Error error
+}
+
+type ICreateUserUseCase interface {
+	CreateUser(input CreateUserInput) error
+}
+
+type createUserUseCase struct {
+	userRepository entity.IUserRepository
+}
+
+func NewCreateUserUseCase(userRepository entity.IUserRepository) ICreateUserUseCase {
+	return &createUserUseCase{
+		userRepository: userRepository,
+	}
+}
+
+func (c *createUserUseCase) CreateUser(input CreateUserInput) error {
+	user, err := entity.NewUserFactory(input.Email, input.Password, input.FirstName, input.LastName)
+	if err != nil {
+		return nil
+	}
+
+	err = c.userRepository.Create(user)
+	if err != nil {
+		return nil
+	}
+
+	return nil
+}
