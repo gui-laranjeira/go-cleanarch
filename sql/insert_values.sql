4339df8e5d87be252f650026995ecb3b2b6991e2
gui-laranjeira <guilr14@gmail.com>
2023-10-15 14:21:09 -0300

    fix: fixing wrong password column name in docker setup sql script

--- sql/insert_values.sql
+++ sql/insert_values.sql
@@ -6,7 +6,7 @@ INSERT INTO public.books (id_book,title,author,pages,publisher,"year",isbn,creat
 	 ('cc73ad03-55c8-4c7d-9ef8-53911b486329','Data Structures and Algorithms with Golang','Bhagvan Kommadi',336,'Packt Publishing',2019,'9781789618419','2023-10-07 22:49:04.2975299-03:00','0001-01-01 00:00:00Z'),
 	 ('a37a2612-f192-4bf9-9ab1-2196f24ede8f','Cybersecurity','Duane C. Wilson',160,'MIT Press',2021,'9780262542548','2023-10-07 22:50:14.3486669-03:00','0001-01-01 00:00:00Z');
 
-INSERT INTO users (id_user,email,phone,"password",first_name,last_name,created_at,updated_at) VALUES
+INSERT INTO users (id_user,email,phone,password,first_name,last_name,created_at,updated_at) VALUES
 	 ('a4ce9866-c04e-4026-8ea9-d6adf761e2b8','teste2@teste.com','987654321','$2a$10$0C4yIL1XxxxhYqnCtsSfF./Ls.uuMLxh5NrFsnFt71945baHSLTvW','Maria','Santo','2023-10-12 21:10:34.679668','0001-01-01 00:00:00'),
 	 ('a4494950-e5f9-4139-8d5e-de498cfcf08b','teste@teste.com','987654321','$2a$10$XTbSu2UgADI8ydhBKWRk4eXqmcNKpCLjrxQz/NvXkONt7t9gRov0q','John','Doe','2023-10-12 21:10:43.495739','0001-01-01 00:00:00');
 
