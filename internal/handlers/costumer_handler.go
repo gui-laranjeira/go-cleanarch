10c950f90fa02cf57ff081b682cbb8b93d593683
gui-laranjeira <guilr14@gmail.com>
2023-10-14 22:56:49 -0300

    feat: add costumer get all and get by id handlers

--- internal/handlers/costumer_handler.go
+++ internal/handlers/costumer_handler.go
@@ -72,9 +72,50 @@ func UpdateCostumerHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetAllCostumersHandler(w http.ResponseWriter, r *http.Request) {
+	db, err := db.OpenConnection()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Printf("Error while opening connection: %v", err)
+		return
+	}
+	repo := repository.NewCostumerSQLRepository(db)
+	uc := usecases.NewFindAllCostumersUseCase(repo)
 
+	output, err := uc.FindAllCostumers()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Printf("Error while getting costumers: %v", err)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(output)
 }
 
 func GetCostumerByIDHandler(w http.ResponseWriter, r *http.Request) {
+	db, err := db.OpenConnection()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Printf("Error while opening connection: %v", err)
+		return
+	}
+	repo := repository.NewCostumerSQLRepository(db)
+	uc := usecases.NewFindCostumerByIDUseCase(repo)
 
+	id := chi.URLParam(r, "id")
+
+	var input usecases.FindCostumerByIDInput
+	input.ID = id
+
+	output, err := uc.FindCostumerByID(input)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Printf("Error while finding costumer: %v", err)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(output)
 }
